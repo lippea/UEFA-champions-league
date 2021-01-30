4611a6b9aacae9f81dfb2ad5844b925bd989b0d9
Lucy HUANG <lippea@gmail.com>
2021-01-30 09:26:11 +0000

    change grouping to objects list with print extra spece after group name

--- group_stage.py
+++ group_stage.py
@@ -2,62 +2,77 @@ import common
 
 print("\033[1;34;40mUEFA CHAMPIONS LEAGUE\033[1;37;40m\n\n")
 
-# points in group stage for each team
-points = [
-  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
-  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0
-]
-
 # 32 teams diviced into 8 groups
-groups = (
-  range(0,4), range(4,8), range(8,12), range(12,16),
-  range(16,20), range(20,24), range(24,28), range(28,32)
-)
+# groups = (
+#   range(0,4), range(4,8), range(8,12), range(12,16),
+#   range(16,20), range(20,24), range(24,28), range(28,32)
+# )
+groups = []
+
 
 # calculate points after a match
-def calculate_points(home_index, home_score, away_index, away_score):
+def calculate_points(home_team, home_score, away_team, away_score):
   if (home_score == away_score):
-    points[home_index] += 1
-    points[away_index] += 1
+    home_team['points'] += 1
+    away_team['points'] += 1
   elif (home_score > away_score):
-    points[home_index] += 3
+    home_team['points'] += 3
   else:
-    points[away_index] += 3
+    away_team['points'] += 3
 
-def match(home_index, away_index):
+def match(home_team, away_team):
   # get scores for two teams
   home_score = common.get_score()
   away_score = common.get_score()
 
   # add to history
-  common.add_results(home_index, home_score, away_index, away_score)
+  common.add_results(home_team['index'], home_score, away_team['index'], away_score)
 
   # add points
-  calculate_points(home_index, home_score, away_index, away_score)
+  calculate_points(home_team, home_score, away_team, away_score)
 
+def get_points(e):
+  return e['points']
 def print_points():
   for group in range(0,8):
-    print(f'\ngroup_{chr(97+group)}', end="")
+    print(f'\ngroup_{chr(97+group)}')
     group_teams = groups[group]
+    #sort by points desc
+    group_teams.sort(key=get_points, reverse=True)
     
     for team in range(0,4):
-      print('{:>30}'.format(f'{common.TEAMS[group_teams[team]]} {points[group_teams[team]]}'), end="")
+      team_index = group_teams[team]['index']
+      team_points = group_teams[team]['points']
+      print('{:>30}'.format(f'{common.TEAMS[team_index]} {team_points}'), end="")
 
   print('\n')
 
+def initial_grouping():
+  #8 groups
+  for group in range(0, 8):
+    #4 teams in a group
+    grouping = []
+    for team in range(0, 4):
+      grouping.append(({'index': group*4+team, 'points': 0}))
+      # grouping.append((group*4)+(team)
+    
+    groups.append(grouping)
+
 def group_stage():
+  initial_grouping()
+
   round1_arragement = ((0, 1), (2, 0), (0, 3), (1, 2) ,(1, 3), (2, 3))
   round2_arragement = ((3, 2), (3, 1), (1, 2), (3, 0) ,(2, 0), (0, 1))
 
   for day in range(0,6):
     for group in range(0,8):
       group_teams = groups[group]
-      home_index = group_teams[round1_arragement[day][0]]
-      away_index = group_teams[round1_arragement[day][1]]
-      match(home_index, away_index)
+      home_team = group_teams[round1_arragement[day][0]]
+      away_team = group_teams[round1_arragement[day][1]]
+      match(home_team, away_team)
 
-      home_index = group_teams[round2_arragement[day][0]]
-      away_index = group_teams[round2_arragement[day][1]]
-      match(home_index, away_index)
+      home_team = group_teams[round2_arragement[day][0]]
+      away_team = group_teams[round2_arragement[day][1]]
+      match(home_team, away_team)
 
   print_points()
