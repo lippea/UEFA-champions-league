f8dd860c5475dcfd524a49f722b9cec4645dd773
Lucy HUANG <lippea@gmail.com>
2021-03-14 23:36:53 +1100

    finalise teams to next phase

--- group_stage/main.py
+++ group_stage/main.py
@@ -3,17 +3,17 @@ TOP_32_TEAMS = ('Bayern', 'Atleti', 'RB Salzburg', 'Lokomotiv', 'Real Madrid', '
 import initialise
 import match
 import sort
+import finalise
 import output
 
 def process_stage(top_32_teams):
   initilised_groups = initialise.random_group(top_32_teams)
-  print(initilised_groups)
 
   groups_with_points = match.round_robin(initilised_groups)
-  print(groups_with_points)
 
   groups_with_points_desc = sort.sort_by_points_desc(groups_with_points)
-  print(groups_with_points_desc)
+
+  next_phase_teams = finalise.get_next_phase_teams(groups_with_points_desc)
 
   output.print_result(groups_with_points_desc)
 
