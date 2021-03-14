13192fbcd3890770d66af93dbed0ca9e7a955f93
Lucy HUANG <lippea@gmail.com>
2021-03-14 23:13:51 +1100

    finish group stage match

--- group_stage/main.py
+++ group_stage/main.py
@@ -1,9 +1,13 @@
 TOP_32_TEAMS = ('Bayern', 'Atleti', 'RB Salzburg', 'Lokomotiv', 'Real Madrid', 'M. Gladbach', 'Shakhtar', 'Inter Milan', 'Man City', 'Porto', 'Olympiacos', 'Marseille', 'Liverpool', 'Atalanta', 'Ajax', 'Midtjylland', 'Chelsea', 'Sevilla', 'Krasnodar', 'Rennes', 'Dortmund', 'Lazio', 'Club Brugge', 'Zenit', 'Juventus', 'Barcelona', 'Dynamo Kiev', 'Ferencváros', 'Paris SG', 'RB Leipzig', 'Man United', 'İstanbul')
 
 import initialise
+import match
 
 def process_stage(top_32_teams):
   initilised_groups = initialise.random_group(top_32_teams)
   print(initilised_groups)
+
+  groups_with_points = match.round_robin(initilised_groups)
+  print(groups_with_points)
 
 process_stage(TOP_32_TEAMS)
