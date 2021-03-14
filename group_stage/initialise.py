a8c82aba2214729f655cc8ece5433a10a1bc1341
Lucy HUANG <lippea@gmail.com>
2021-03-14 22:43:41 +1100

    random group done

---
+++ group_stage/initialise.py
@@ -0,0 +1,16 @@
+import random
+
+def random_group(top_32_teams):
+  teams = list(top_32_teams)
+  groups = []
+  #8 groups
+  for group in range(0, 8):
+    #4 teams in a group
+    grouping = []
+    for team in range(0, 4):
+      random_team = teams.pop(random.randrange(len(teams))) 
+      grouping.append(({'team': random_team, 'points': 0}))
+    
+    groups.append(grouping)
+
+  return groups
---
+++ group_stage/main.py
@@ -0,0 +1,9 @@
+TOP_32_TEAMS = ('Bayern', 'Atleti', 'RB Salzburg', 'Lokomotiv', 'Real Madrid', 'M. Gladbach', 'Shakhtar', 'Inter Milan', 'Man City', 'Porto', 'Olympiacos', 'Marseille', 'Liverpool', 'Atalanta', 'Ajax', 'Midtjylland', 'Chelsea', 'Sevilla', 'Krasnodar', 'Rennes', 'Dortmund', 'Lazio', 'Club Brugge', 'Zenit', 'Juventus', 'Barcelona', 'Dynamo Kiev', 'Ferencváros', 'Paris SG', 'RB Leipzig', 'Man United', 'İstanbul')
+
+import initialise
+
+def process_stage(top_32_teams):
+  initilised_groups = initialise.random_group(top_32_teams)
+  print(initilised_groups)
+
+process_stage(TOP_32_TEAMS)
